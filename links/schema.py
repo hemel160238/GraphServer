1d2a3a1d82a1f320e7e16e77abbbc55aff9516af
hemel160238 <tanvirhemel9208@gmail.com>
2020-02-18 01:04:18 +0600

    Add Mutation

--- links/schema.py
+++ links/schema.py
@@ -8,9 +8,32 @@ class LinkType(DjangoObjectType):
     class Meta:
         model = Link
 
-
 class Query(graphene.ObjectType):
     links = graphene.List(LinkType)
 
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
+
+#1
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    #2  Deines Data that can be sent to the Server, Here url and description can be sent
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    #3 The Mutation Method
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        new_link = CreateLink(id= link.id, url = link.url, description = link.description)
+
+        return new_link
+
+#4
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
